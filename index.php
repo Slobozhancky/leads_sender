2a68ec49ec90e8966a5a16b3e07f476692b78f94
Slobozhancky <il.slob93@gmail.com>
2024-04-11 19:47:13 +0300

    added some changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__DIR__) . "/config/constants.php";
+require_once __DIR__ . "/config/constants.php";
 require_once ROOT . "/vendor/autoload.php";
 require_once CONFIG . "/func.php";
 
